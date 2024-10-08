fe1f0d9c74b303803a5f8e6b1449d17df9990f03
DiegoRicaldezAguilar <rad1010553@est.univalle.edu>
2024-10-08 19:36:05 -0400

    arreglito

--- lib/Service/SubirImagenesService.dart
+++ lib/Service/SubirImagenesService.dart
@@ -6,44 +6,31 @@ class SubirImagenesService {
   final String baseUri = "https://localhost:7096/Images/subirImagen";
 
   Future<bool> subir(SubirImagen imagenSubida) async {
-  final url = Uri.parse(baseUri);
+    final url = Uri.parse(baseUri);
 
-  var request = http.MultipartRequest('POST', url);
-  request.fields['idCompetencia'] = imagenSubida.idCompetencia.toString();
-  request.fields['idEstudiante'] = imagenSubida.idEstudiante.toString();
+    var request = http.MultipartRequest('POST', url);
+    request.fields['idCompetencia'] = imagenSubida.idCompetencia.toString();
+    request.fields['idEstudiante'] = imagenSubida.idEstudiante.toString();
 
-  // Crear el MultipartFile desde Uint8List
-  var multipartFile = http.MultipartFile.fromBytes(
-    'imagen',
-    imagenSubida.imagen,
-    filename: imagenSubida.nombre,
-    contentType: MediaType('application', 'octet-stream'),
-  );
+    request.files.add(http.MultipartFile.fromBytes(
+      'imagen',
+      imagenSubida.imagen,
+      filename: imagenSubida.nombre,
+      contentType:
+          MediaType('image', imagenSubida.nombre.split('.').last),
+    ));
 
-  // Agregar el MultipartFile a la solicitud
-  request.files.add(multipartFile);
+    try {
+      print('Enviando imagen de tamaño: ${imagenSubida.imagen.length} bytes');
+      var response = await request.send();
 
-  try {
-    // Imprimir para depuración
-    print('Enviando imagen de tamaño: ${imagenSubida.imagen.length} bytes');
-
-    // Enviar la solicitud
-    var response = await request.send();
-
-    // Procesar la respuesta
-    if (response.statusCode == 200) {
-      print('Imagen enviada correctamente.');
-      return true;
-    } else {
-      // Manejar el error y obtener el mensaje de respuesta
-      final responseData = await response.stream.bytesToString();
-      print('Error: $responseData');
+      if (response.statusCode == 200) {
+        return true;
+      }         
+      
+      return false;
+    } catch (e) {
       return false;
     }
-  } catch (e) {
-    // Manejar excepciones
-    print('Excepción: $e');
-    return false;
   }
-}
 }
