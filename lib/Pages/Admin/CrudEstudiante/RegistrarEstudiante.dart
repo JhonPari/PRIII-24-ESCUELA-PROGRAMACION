6ac3e6311d70ebb513b32ed75c355b3ac8405f00
JhonPari <psj0031943@est.univalle.edu>
2024-11-05 13:53:10 -0400

    falta

--- lib/Pages/Admin/CrudEstudiante/RegistrarEstudiante.dart
+++ lib/Pages/Admin/CrudEstudiante/RegistrarEstudiante.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:prlll_24_escuela_programacion/Pages/Navbar/AdminNavBar.dart';
 import 'package:prlll_24_escuela_programacion/Service/usuarios_service.dart';
@@ -75,7 +73,6 @@ class _RegistrarEstState extends State<RegistrarEstPage> {
 
         Usuario estudiante = await _usuarioService.post(nuevoUsuario);
 
-        // Mostrar cuadro de diálogo de éxito
         showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -91,13 +88,10 @@ class _RegistrarEstState extends State<RegistrarEstPage> {
               actions: [
                 TextButton(
                   onPressed: () {
-                    Navigator.of(context).pop(); // Cerrar el cuadro de diálogo
-                    
+                    Navigator.of(context).pop();
                     _nombreController.clear();
                     _correoController.clear();
-                    
-                    Navigator.of(context)
-                        .pop(); 
+                    Navigator.of(context).pop();
                   },
                   child: const Text('OK'),
                 ),
@@ -106,15 +100,10 @@ class _RegistrarEstState extends State<RegistrarEstPage> {
           },
         );
       } catch (e) {
-        
         String errorMessage = "Error al crear el usuario";
-
-        
         if (e.toString().contains("El correo ya está en uso")) {
           errorMessage = "El correo ya está en uso. Intente con otro.";
         }
-
-        // Mostrar cuadro de diálogo de error
         showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -130,8 +119,7 @@ class _RegistrarEstState extends State<RegistrarEstPage> {
               actions: [
                 TextButton(
                   onPressed: () {
-                    Navigator.of(context).pop(); 
-                    
+                    Navigator.of(context).pop();
                     _nombreController.clear();
                     _correoController.clear();
                   },
@@ -143,7 +131,6 @@ class _RegistrarEstState extends State<RegistrarEstPage> {
         );
       }
     } else {
-     
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -159,7 +146,7 @@ class _RegistrarEstState extends State<RegistrarEstPage> {
             actions: [
               TextButton(
                 onPressed: () {
-                  Navigator.of(context).pop(); // Cerrar el cuadro de diálogo
+                  Navigator.of(context).pop();
                 },
                 child: const Text('OK'),
               ),
@@ -172,89 +159,96 @@ class _RegistrarEstState extends State<RegistrarEstPage> {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: adminNavBar(
-          name ?? '...', storage, context), 
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              const SizedBox(height: 30),
-              const Text(
-                'REGISTRAR ESTUDIANTE',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF8E244D),
-                ),
-              ),
-              const SizedBox(height: 30),
-              Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFE0BFC7),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: _nombreController,
-                      validator: validateFullName,
-                      decoration: InputDecoration(
-                        labelText: 'Nombres',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
+      appBar: adminNavBar(name ?? '...', storage, context),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 30),
+                  const Text(
+                    'REGISTRAR ESTUDIANTE',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF8E244D),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFE0BFC7),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    width: screenWidth > 600 ? 600 : screenWidth * 0.9,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _nombreController,
+                          validator: validateFullName,
+                          decoration: InputDecoration(
+                            labelText: 'Nombres',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            filled: true,
+                            fillColor: const Color(0xFFF5E0E5),
+                          ),
                         ),
-                        filled: true,
-                        fillColor: const Color(0xFFF5E0E5),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      controller: _correoController,
-                      validator: validateEmail,
-                      decoration: InputDecoration(
-                        labelText: 'Correo',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: _correoController,
+                          validator: validateEmail,
+                          decoration: InputDecoration(
+                            labelText: 'Correo',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            filled: true,
+                            fillColor: const Color(0xFFF5E0E5),
+                          ),
                         ),
-                        filled: true,
-                        fillColor: const Color(0xFFF5E0E5),
-                      ),
+                        const SizedBox(height: 20),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            crearUsuario(context);
+                          },
+                          icon: const Icon(Icons.check, color: Colors.white),
+                          label: const Text(
+                            'Aceptar',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF8E244D),
+                            minimumSize: const Size(double.infinity, 40),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey,
+                            minimumSize: const Size(double.infinity, 40),
+                          ),
+                          child: const Text('Volver',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 20),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        crearUsuario(context);
-                      },
-                      icon: const Icon(Icons.check, color: Colors.white),
-                      label: const Text(
-                        'Aceptar',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF8E244D),
-                        minimumSize: const Size(double.infinity, 40),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey,
-                        minimumSize: const Size(double.infinity, 40),
-                      ),
-                      child: const Text('Volver',
-                          style: TextStyle(color: Colors.white)),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
